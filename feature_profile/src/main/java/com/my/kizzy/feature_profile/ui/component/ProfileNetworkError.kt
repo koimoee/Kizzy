7813ba2392de01c90d73584046caedd9ee370f15
DreamiiChu Ch. <107134739+koimoee@users.noreply.github.com>
2024-12-25 23:16:30 +0700

    fix(ProfileNetworkError): fixed missing import cause an error unresolved

--- feature_profile/src/main/java/com/my/kizzy/feature_profile/ui/component/ProfileNetworkError.kt
+++ feature_profile/src/main/java/com/my/kizzy/feature_profile/ui/component/ProfileNetworkError.kt
@@ -27,6 +27,10 @@ import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
